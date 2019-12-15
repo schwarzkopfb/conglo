3ac7be09ebb44a102b48c02383f8c4f824bc87e5
Balazs Schwarzkopf <schwarzkopfb@icloud.com>
2019-12-15 23:44:43 +0100

    test(ctor): cover `AsyncIterable`

--- test/AsyncIterable.js
+++ test/AsyncIterable.js
@@ -1,6 +1,23 @@
 'use strict'
 
 const test = require('tap'),
+      { inspect } = require('util'),
       AsyncIterable = require('../lib/AsyncIterable')
 
-test.ok('ok for now')
+test.resolveMatch(new AsyncIterable().toArray(), [] , 'should be instantiated without args')
+test.resolveMatch(new AsyncIterable(42).toArray(), [ 42 ] , 'constructor should respect args')
+test.resolveMatch(new AsyncIterable([ 6, 9 ]).toArray(), [ 6, 9 ] , 'constructor should respect args #2')
+test.throws(() => AsyncIterable('something'), Error, '`new` keyword is required to instantiate an `AsyncIterable`')
+test.same(inspect(new AsyncIterable()), 'AsyncIterable {}', 'inspection should not cause errors')
+test.throws(() => { for (const item of new AsyncIterable); }, Error, 'sync iteration of an AsyncIterable is not possible')
+test.resolveMatch(
+    async () => {
+        const arr = []
+
+        for await (const item of new AsyncIterable([ 3, 1, 2 ]))
+            arr.push(item)
+
+        return arr
+    }, 
+    [ 3, 1, 2 ], 'async iteration of an AsyncIterable should work'
+)
